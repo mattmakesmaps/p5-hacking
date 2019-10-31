6cb55df87a496feb4a049faf81b8a92d0f2b82d4
Matthew Kenny <matthewkenny@gmail.com>
2019-10-30 20:21:27 -0700

    add jitter to the rotatingRectangle example

--- empty-example/rotatingRectangles.js
+++ empty-example/rotatingRectangles.js
@@ -2,7 +2,8 @@ let config = {
     numCols: 20,
     numRows: 15,
     recWidth: 30,
-    recHeight: 5 
+    recHeight: 5,
+    noiseSeed: 0.0
 }
 
 function setup() {
@@ -24,11 +25,24 @@ function drawRectangleRotation(x = 0, y = 0) {
     let aX = (config.recWidth / 2) + x;
     let aY = (config.recWidth / 2) + y;
     let aAngle = atan2(mouseY - aY, mouseX - aX);
+
+    config.noiseSeed = config.noiseSeed + 0.0002;
+    let noiseCalc = noise(config.noiseSeed);
+
+    // Map the noiseCalc value to a range
+    // that is quarter pi above or below current aAngle
+    // Then rotate by that.
+    const jitterScale = QUARTER_PI;
+    let jitter = map(noiseCalc, 0, 1, aAngle - jitterScale, aAngle + jitterScale);
+
     // REF: https://stats.stackexchange.com/questions/70801/how-to-normalize-data-to-0-1-range
     // The range for atan2() is -PI to PI;
-    let normalizedAngle = (aAngle - -PI) / TWO_PI;
+    // let normalizedAngle = (aAngle - -PI) / TWO_PI;
+    // `map()` is analagous to above
+    let normalizedAngle = map(aAngle, -PI, PI, 0, 1);
+
     translate(aX, aY);
-    rotate(aAngle);
+    rotate(jitter);
     fill(calcColor(normalizedAngle));
     rect(
         (-config.recWidth / 2),
