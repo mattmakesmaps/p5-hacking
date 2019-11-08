3e02a37ab63053cdff12e2cd02c93f9e3ad6059e
Matthew Kenny <matthewkenny@gmail.com>
2019-11-08 15:02:19 -0800

    vector ball is now moving when hit.

--- empty-example/rotationWithVector.js
+++ empty-example/rotationWithVector.js
@@ -6,12 +6,15 @@
  * position, which is used to rotate the line.
  */
 let ball = {
-    x: 10,
-    y: 10
+    hit: false,
+    position: null,
+    velocity: 5,
+    speed: 3.5
 }
 
 function setup() {
     createCanvas(700, 600);
+    ball.position = createVector(20, 20);
 }
 
 function linePoint(x1, y1, x2, y2, px, py) {
@@ -46,8 +49,7 @@ function draw() {
     let lineLength = 100;
     translate(width/2,height/2);
     strokeWeight(10);
-    let pVect = createVector(ball.x,ball.y);
-    point(pVect.x, pVect.y);
+    point(ball.position.x, ball.position.y);
 
     // Create a 100 pixel line.
     let v1 = createVector(0 - (lineLength/2), 0);
@@ -65,22 +67,27 @@ function draw() {
     text('Heading: ' + mouseVector.heading(), 100, 100);
     text('v1 x/y: ' + v1.x + ', ' + v1.y, 100, 120);
     text('v2 x/y: ' + v2.x + ', ' + v2.y, 100, 140);
-    text('pVect x/y: ' + pVect.x + ', ' + pVect.y, 100, 160);
-    text('ball x/y: ' + ball.x + ', ' + ball.y, 100, 180);
+    // text('pVect x/y: ' + pVect.x + ', ' + pVect.y, 100, 160);
+    text('ball.position x/y: ' + ball.position.x + ', ' + ball.position.y, 100, 180);
     strokeWeight(3);
 
     // rather the rotating the canvas, we rotate the vectors.
     v1.rotate(mouseVector.heading());
     v2.rotate(mouseVector.heading());
 
-    let hit = linePoint(v1.x,v1.y, v2.x, v2.y, pVect.x,pVect.y);
+    let hit = linePoint(v1.x,v1.y, v2.x, v2.y, ball.position.x,ball.position.y);
     if (hit) {
-        ball.x = ball.x + 1;
-        ball.y = ball.y + 1;
+        ball.hit = true;
+        // ball.position.x = ball.position.x + 1;
+        // ball.position.y = ball.position.y + 0.5;
         stroke(255,150,0, 150);
     }
     else {
         stroke(0,150,255, 150);
+    }
+
+    if (ball.hit) {
+        ball.position.add(ball.velocity);
     }
     
     // Draw the line now that we've rotated the canvas.
