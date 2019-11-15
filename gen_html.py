1823a2052967b9fc0e9c3e076081bbe8adcdf90a
Matthew Kenny <matthewkenny@gmail.com>
2019-11-14 20:56:03 -0800

    Auto generate `index.html`

--- gen_html.py
+++ gen_html.py
@@ -1,11 +1,26 @@
 import os
 from string import Template
 
-html_template = Template("""<DOCTYPE html>
+htmlTemplateIndex = Template("""<DOCTYPE html>
 <html lang="">
   <head>
     <meta charset="utf-8">
-    <title>p5.js example</title>
+    <title>Examples</title>
+  </head>
+  <body>
+    <ul>
+        ${htmlFiles}
+    </ul>
+  </body>
+</html>""")
+
+htmlTemplateIndexLinkElement = Template('<li><a href="${relURL}">${exampleName}</a></li>\n')
+
+htmlTemplateExample = Template("""<DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="utf-8">
+    <title>${fileName}</title>
     <style> body {padding: 0; margin: 0;} canvas {vertical-align: top;} </style>
     <script src="../p5/p5.min.js"></script>
     <script src="../addons/p5.dom.min.js"></script>
@@ -22,12 +37,29 @@ if __name__ == "__main__":
     # Get JS Files 
     examplePath = os.path.join(os.getcwd(), 'examples')
     jsFiles = [f for f in os.listdir(examplePath) if f.endswith('js')]
-    print(jsFiles)
+
+    htmlFilesString = ''
 
     for jsFilename in jsFiles:
+        # Create links to the HTML pages for each example 
+        # To be used in index.html
         htmlFilename = jsFilename.replace('.js', '.html')
-        htmlPath = os.path.join(examplePath, htmlFilename)
+        htmlLinkElement = htmlTemplateIndexLinkElement.substitute({
+            'relURL': '/examples/' + htmlFilename,
+            'exampleName': htmlFilename
+        })
+        htmlFilesString = htmlFilesString + htmlLinkElement
 
+        # Create the HTML pages for each example
+        htmlPath = os.path.join(examplePath, htmlFilename)
         with open(htmlPath, 'w') as htmlFileHandle:
-            contents = html_template.substitute({'fileName': jsFilename})
+            contents = htmlTemplateExample.substitute({'fileName': jsFilename})
             htmlFileHandle.write(contents)
+
+    # Create index.html
+    htmlIndexFilePath = os.path.join(os.getcwd(), 'index.html')
+    with open(htmlIndexFilePath, 'w') as htmlIndexFileHandle:
+        contents = htmlTemplateIndex.substitute({'htmlFiles': htmlFilesString})
+        htmlIndexFileHandle.write(contents)
+    
+    print('Done')
